b1bf66dc7bcb25a5bc04be4d2edf339a0240764a
chrisdews <cldews1@gmail.com>
2019-08-29 18:43:34 +0100

    added umusambi

--- js/dewsy.js
+++ js/dewsy.js
@@ -6,7 +6,29 @@ $(window).scroll(function(){
         topDist = $elem.offset().top - $(this).scrollTop(),
         bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
     
-    if ((topDist - 350) < offset && (bottomDist -200 ) > offset) {
+    if ((topDist - 450) < offset && (bottomDist -100) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade1a"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 450) < offset && (bottomDist -100 ) > offset) {
         // (turn on colors)
         // $elem.css("background-color","red");
         $elem.css({'-webkit-filter': 'grayscale(0%)', 
@@ -28,7 +50,7 @@ $(window).scroll(function(){
         topDist = $elem.offset().top - $(this).scrollTop(),
         bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
     
-    if ((topDist - 350) < offset && (bottomDist - 200) > offset) {
+    if ((topDist - 350) < offset && (bottomDist) > offset) {
         // (turn on colors)
         // $elem.css("background-color","red");
         $elem.css({'-webkit-filter': 'grayscale(0%)', 
@@ -50,7 +72,7 @@ $(window).scroll(function(){
         topDist = $elem.offset().top - $(this).scrollTop(),
         bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
     
-    if ((topDist - 450) < offset && (bottomDist) > offset) {
+    if ((topDist - 450) < offset && (bottomDist -200) > offset) {
         // (turn on colors)
         // $elem.css("background-color","red");
         $elem.css({'-webkit-filter': 'grayscale(0%)', 
