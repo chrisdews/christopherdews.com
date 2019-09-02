06b0564135196ebf31b150d4fb857b3d856a01d7
chrisdews <cldews1@gmail.com>
2019-09-02 23:41:02 +0100

    added projects

--- js/dewsy.js
+++ js/dewsy.js
@@ -110,3 +110,25 @@ $(window).scroll(function(){
     }
 });
 
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade5"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
