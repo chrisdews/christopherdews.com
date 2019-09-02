a2b21a1b54c21e97dd14d3aba39ffef151689832
chrisdews <cldews1@gmail.com>
2019-09-03 00:44:06 +0100

    refactored js fade

--- js/dewsy.js
+++ js/dewsy.js
@@ -2,95 +2,75 @@ var offset = 100;
 
 $(window).scroll(function(){
     
-    var $elem = $(".greyfade1"),
-        topDist = $elem.offset().top - $(this).scrollTop(),
-        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
-    
-    if ((topDist - 450) < offset && (bottomDist -100) > offset) {
-        // (turn on colors)
-        // $elem.css("background-color","red");
-        $elem.css({'-webkit-filter': 'grayscale(0%)', 
-                    '-moz-filter': 'grayscale(0%)',
-                    'filter': 'grayscale(0%)',
-                    'transition': 'all 0.5s ease'});
-    } else {
-        // (turn off colors)
-        $elem.css({'-webkit-filter': 'grayscale(100%)', 
-                    '-moz-filter': 'grayscale(100%)',
-                    'filter': 'grayscale(100%)',
-                    'transition': 'all 0.5s ease'});
-    }
+    var $elem = $(".greyfade1 img"),
+    topDist = $elem.offset().top - $(this).scrollTop(),
+    bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+
+if ((topDist - 650) < offset && (bottomDist) > offset) {
+    // (turn on colors)
+    // $elem.css("background-color","red");
+    $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                '-moz-filter': 'grayscale(0%)',
+                'filter': 'grayscale(0%)',
+                'transition': 'all 0.5s ease'});
+} else {
+    // (turn off colors)
+    $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                '-moz-filter': 'grayscale(100%)',
+                'filter': 'grayscale(100%)',
+                'transition': 'all 0.5s ease'});
+}
 });
 
 $(window).scroll(function(){
     
-    var $elem = $(".greyfade1a"),
-        topDist = $elem.offset().top - $(this).scrollTop(),
-        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
-    
-    if ((topDist - 450) < offset && (bottomDist -100 ) > offset) {
-        // (turn on colors)
-        // $elem.css("background-color","red");
-        $elem.css({'-webkit-filter': 'grayscale(0%)', 
-                    '-moz-filter': 'grayscale(0%)',
-                    'filter': 'grayscale(0%)',
-                    'transition': 'all 0.5s ease'});
-    } else {
-        // (turn off colors)
-        $elem.css({'-webkit-filter': 'grayscale(100%)', 
-                    '-moz-filter': 'grayscale(100%)',
-                    'filter': 'grayscale(100%)',
-                    'transition': 'all 0.5s ease'});
-    }
+    var $elem = $(".greyfade1a img"),
+    topDist = $elem.offset().top - $(this).scrollTop(),
+    bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+
+if ((topDist - 650) < offset && (bottomDist) > offset) {
+    // (turn on colors)
+    // $elem.css("background-color","red");
+    $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                '-moz-filter': 'grayscale(0%)',
+                'filter': 'grayscale(0%)',
+                'transition': 'all 0.5s ease'});
+} else {
+    // (turn off colors)
+    $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                '-moz-filter': 'grayscale(100%)',
+                'filter': 'grayscale(100%)',
+                'transition': 'all 0.5s ease'});
+}
 });
 
 $(window).scroll(function(){
     
-    var $elem = $(".greyfade2"),
-        topDist = $elem.offset().top - $(this).scrollTop(),
-        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
-    
-    if ((topDist - 350) < offset && (bottomDist) > offset) {
-        // (turn on colors)
-        // $elem.css("background-color","red");
-        $elem.css({'-webkit-filter': 'grayscale(0%)', 
-                    '-moz-filter': 'grayscale(0%)',
-                    'filter': 'grayscale(0%)',
-                    'transition': 'all 0.5s ease'});
-    } else {
-        // (turn off colors)
-        $elem.css({'-webkit-filter': 'grayscale(100%)', 
-                    '-moz-filter': 'grayscale(100%)',
-                    'filter': 'grayscale(100%)',
-                    'transition': 'all 0.5s ease'});
-    }
+    var $elem = $(".greyfade2 img"),
+    topDist = $elem.offset().top - $(this).scrollTop(),
+    bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+
+if ((topDist - 650) < offset && (bottomDist) > offset) {
+    // (turn on colors)
+    // $elem.css("background-color","red");
+    $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                '-moz-filter': 'grayscale(0%)',
+                'filter': 'grayscale(0%)',
+                'transition': 'all 0.5s ease'});
+} else {
+    // (turn off colors)
+    $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                '-moz-filter': 'grayscale(100%)',
+                'filter': 'grayscale(100%)',
+                'transition': 'all 0.5s ease'});
+}
 });
+
+
 
 $(window).scroll(function(){
     
-    var $elem = $(".greyfade3"),
-        topDist = $elem.offset().top - $(this).scrollTop(),
-        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
-    
-    if ((topDist - 450) < offset && (bottomDist -200) > offset) {
-        // (turn on colors)
-        // $elem.css("background-color","red");
-        $elem.css({'-webkit-filter': 'grayscale(0%)', 
-                    '-moz-filter': 'grayscale(0%)',
-                    'filter': 'grayscale(0%)',
-                    'transition': 'all 0.5s ease'});
-    } else {
-        // (turn off colors)
-        $elem.css({'-webkit-filter': 'grayscale(100%)', 
-                    '-moz-filter': 'grayscale(100%)',
-                    'filter': 'grayscale(100%)',
-                    'transition': 'all 0.5s ease'});
-    }
-});
-
-$(window).scroll(function(){
-    
-    var $elem = $(".greyfade4"),
+    var $elem = $(".greyfade4 img"),
         topDist = $elem.offset().top - $(this).scrollTop(),
         bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
     
@@ -113,7 +93,117 @@ $(window).scroll(function(){
 
 $(window).scroll(function(){
     
-    var $elem = $(".greyfade5"),
+    var $elem = $(".greyfade5 img"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade6 img"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade7 img"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade10 img"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade11 img"),
+        topDist = $elem.offset().top - $(this).scrollTop(),
+        bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
+    
+    if ((topDist - 650) < offset && (bottomDist) > offset) {
+        // (turn on colors)
+        // $elem.css("background-color","red");
+        $elem.css({'-webkit-filter': 'grayscale(0%)', 
+                    '-moz-filter': 'grayscale(0%)',
+                    'filter': 'grayscale(0%)',
+                    'transition': 'all 0.5s ease'});
+    } else {
+        // (turn off colors)
+        $elem.css({'-webkit-filter': 'grayscale(100%)', 
+                    '-moz-filter': 'grayscale(100%)',
+                    'filter': 'grayscale(100%)',
+                    'transition': 'all 0.5s ease'});
+    }
+});
+
+$(window).scroll(function(){
+    
+    var $elem = $(".greyfade12 img"),
         topDist = $elem.offset().top - $(this).scrollTop(),
         bottomDist = $elem.offset().top + $elem.height() - $(this).scrollTop();
     
